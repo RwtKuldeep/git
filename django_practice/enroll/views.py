5faf60c4b51a3223d554f4658564c53d61409871
“kuldeep <surajaryaa31@gmail.com>
2023-12-12 10:40:25 +0530

    changes in view.py , url.py and index.html files

--- django_practice/enroll/views.py
+++ django_practice/enroll/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Student
+from .forms import Student_info
 # Create your views here.
+
+
+def home(request):
+    fm = Student_info()
+    return render(request, 'enroll/index.html', {'form': fm})
