6a2a25ba075e538fe466038fb4ccfad53949f863
“kuldeep <surajaryaa31@gmail.com>
2023-12-12 11:04:49 +0530

    add widgets in forms.py

--- django_practice/enroll/forms.py
+++ django_practice/enroll/forms.py
@@ -6,3 +6,8 @@ class Student_info(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'email', 'phone_no']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Your Name', 'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter Your Name', 'class': 'form-control'}),
+            'phone_no': forms.NumberInput(attrs={'placeholder': 'Enter Your Name', 'class': 'form-control'})
+        }
